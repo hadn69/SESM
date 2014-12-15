b4c6d1138e6b4119233338f142508bfc68263ecd
Remy GRANDIN <remy.grandin@supinfo.com>
2014-12-16 00:53:16 +0100

    Added a delete all button in the log manager fixed the value of maxplayer, maxfloatingobjects and trashremover being overwriten by changing map

--- SESM/Controllers/LogController.cs
+++ SESM/Controllers/LogController.cs
@@ -27,18 +27,18 @@ namespace SESM.Controllers
             EntityUser user = Session["User"] as EntityUser;
             ServerProvider srvPrv = new ServerProvider(_context);
             EntityServer serv = srvPrv.GetServer(id);
-            if (serv == null)
+            if(serv == null)
             {
                 return RedirectToAction("Index", "Server");
             }
 
             ViewData["ID"] = id;
 
-            string[] listLog = Directory.GetFiles(PathHelper.GetInstancePath(serv),"*.log");
+            string[] listLog = Directory.GetFiles(PathHelper.GetInstancePath(serv), "*.log");
 
             List<SelectListItem> listSLI = new List<SelectListItem>();
 
-            foreach (string item in listLog)
+            foreach(string item in listLog)
             {
                 SelectListItem sli = new SelectListItem();
                 sli.Text = System.IO.File.GetLastWriteTime(item).ToString("dd/MM/yy HH:mm:ss") + " " + PathHelper.GetLastLeaf(item);
@@ -65,17 +65,17 @@ namespace SESM.Controllers
             ViewData["ID"] = id;
             EntityServer serv = srvPrv.GetServer(id);
             string path = PathHelper.GetInstancePath(serv) + model.LogName;
-            if (!ModelState.IsValid)
+            if(!ModelState.IsValid)
             {
-                return RedirectToAction("Index", new {id = id});
+                return RedirectToAction("Index", new { id = id });
             }
-            if (System.IO.File.Exists(path))
+            if(System.IO.File.Exists(path))
             {
                 ViewData["LogName"] = model.LogName;
                 FileStream logFileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
                 StreamReader logFileReader = new StreamReader(logFileStream);
                 List<string> logList = new List<string>();
-                while (!logFileReader.EndOfStream)
+                while(!logFileReader.EndOfStream)
                 {
                     logList.Add(logFileReader.ReadLine());
                 }
@@ -100,7 +100,7 @@ namespace SESM.Controllers
             ServerProvider srvPrv = new ServerProvider(_context);
             EntityServer serv = srvPrv.GetServer(id);
 
-            if (ModelState.IsValid && System.IO.File.Exists(PathHelper.GetInstancePath(serv) + model.LogName))
+            if(ModelState.IsValid && System.IO.File.Exists(PathHelper.GetInstancePath(serv) + model.LogName))
             {
                 System.IO.File.Delete(PathHelper.GetInstancePath(serv) + model.LogName);
                 return RedirectToAction("Index", new { id = id }).Success("Log \"" + model.LogName + "\" deleted");
@@ -121,16 +121,16 @@ namespace SESM.Controllers
             ViewData["ID"] = id;
             EntityServer serv = srvPrv.GetServer(id);
 
-            if (ModelState.IsValid && System.IO.File.Exists(PathHelper.GetInstancePath(serv) + model.LogName))
+            if(ModelState.IsValid && System.IO.File.Exists(PathHelper.GetInstancePath(serv) + model.LogName))
             {
                 Response.Clear();
                 Response.ContentType = "application/zip";
                 Response.AddHeader("Content-Disposition",
                     String.Format("attachment; filename={0}", model.LogName + ".zip"));
 
-                using (ZipFile zip = new ZipFile())
+                using(ZipFile zip = new ZipFile())
                 {
-                    zip.AddFile(PathHelper.GetInstancePath(serv) + model.LogName,"/");
+                    zip.AddFile(PathHelper.GetInstancePath(serv) + model.LogName, "/");
                     zip.Save(Response.OutputStream);
                 }
                 Response.End();
@@ -138,8 +138,6 @@ namespace SESM.Controllers
             return RedirectToAction("Index", new { id = id });
         }
 
-        //
-        // POST: Log/DownloadLog/5
         [HttpGet]
         [ManagerAndAbove]
         public ActionResult DownloadAll(int id)
@@ -149,26 +147,48 @@ namespace SESM.Controllers
             ViewData["ID"] = id;
             EntityServer serv = srvPrv.GetServer(id);
 
-            if (ModelState.IsValid)
-            {
-                Response.Clear();
-                Response.ContentType = "application/zip";
-                Response.AddHeader("Content-Disposition",
-                    String.Format("attachment; filename={0}", serv.Name + "_Logs.zip"));
+            Response.Clear();
+            Response.ContentType = "application/zip";
+            Response.AddHeader("Content-Disposition",
+                String.Format("attachment; filename={0}", serv.Name + "_Logs.zip"));
 
-                using (ZipFile zip = new ZipFile())
-                {
-                    zip.AddFiles(Directory.GetFiles(PathHelper.GetInstancePath(serv), "*.log"),"/");
-                    zip.Save(Response.OutputStream);
-                }
-                Response.End();
+            using(ZipFile zip = new ZipFile())
+            {
+                zip.AddFiles(Directory.GetFiles(PathHelper.GetInstancePath(serv), "*.log"), "/");
+                zip.Save(Response.OutputStream);
             }
+            Response.End();
+
+            return RedirectToAction("Index", new { id = id });
+        }
+
+        [HttpGet]
+        [ManagerAndAbove]
+        public ActionResult DeleteAll(int id)
+        {
+            EntityUser user = Session["User"] as EntityUser;
+            ServerProvider srvPrv = new ServerProvider(_context);
+            ViewData["ID"] = id;
+            EntityServer serv = srvPrv.GetServer(id);
+
+            foreach (string path in Directory.GetFiles(PathHelper.GetInstancePath(serv), "*.log"))
+            {
+                try
+                {
+                    System.IO.File.Delete(path);
+                }
+                catch (Exception)
+                {
+                    
+                }
+            }
+            
             return RedirectToAction("Index", new { id = id });
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
+            if(disposing)
             {
                 _context.Dispose();
             }
