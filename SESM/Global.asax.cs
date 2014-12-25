531f5feffb754f5d6909265cd727eeb59b275e19
Remy GRANDIN <remy.grandin@supinfo.com>
2014-12-25 17:15:30 +0100

    changed version number

--- SESM/Global.asax.cs
+++ SESM/Global.asax.cs
@@ -15,7 +15,7 @@ namespace SESM
     {
         protected void Application_Start()
         {
-            Constants.SetVersion(2, 9, 11);
+            Constants.SetVersion(2, 9, 12);
 
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
