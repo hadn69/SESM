95e9c5150cba7effdc94f35bacda89bd765dd3b5
Rémy GRANDIN <remy.grandin@supinfo.com>
2015-03-21 13:59:21 +0100

    Changed the version number

--- SESM/Global.asax.cs
+++ SESM/Global.asax.cs
@@ -18,11 +18,10 @@ namespace SESM
     {
         protected void Application_Start()
         {
-            Constants.SetVersion(3,0,0);
+            Constants.SetVersion(3,0,1);
             // Resetting Run Vars
             if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"\SESM.RunVar"))
                 File.Delete(AppDomain.CurrentDomain.BaseDirectory + @"\SESM.RunVar");
-
 
             // Registering routes
             AreaRegistration.RegisterAllAreas();
