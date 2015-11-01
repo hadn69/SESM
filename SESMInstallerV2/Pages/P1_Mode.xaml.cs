82f7e2716ff8617871683c61db0e92bdcb97c023
Rémy GRANDIN <remy.grandin@supinfo.com>
2015-11-01 01:46:11 +0100

    Updated SESM installer V2

---
+++ SESMInstallerV2/Pages/P1_Mode.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace SESMInstallerV2.Pages
+{
+    public partial class P1_Mode : MasterUserControl
+    {
+        public P1_Mode()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_Start_Click(object sender, RoutedEventArgs e)
+        {
+            OnMoveForwardTo(new P0_Intro());
+        }
+    }
+}
